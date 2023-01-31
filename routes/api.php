6a5f95649dbfae184f28d8f640d85b8bf930d833
Facu <105363828+Facundoblanco10@users.noreply.github.com>
2023-01-30 23:52:43 -0300

    Commenting and separating routes by Controller
    Nacho's suggestion

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+/**
+ * FeederController routes:
+ */
 Route::get('feeders', [FeederController::class, 'index']);
 Route::post('feeders', [FeederController::class, 'store']);
+
+/UserFeederUserController routes:
+ */
 Route::get('feeder_user', [FeederUserController::class, 'index']);
+
+/**
+ * FeedingTimeController routes:
+ */
 Route::get('feeding_times', [FeedingTimeController::class, 'index']);
 Route::post('feeding_times', [FeedingTimeController::class, 'store']);
+
+/**
+ * FoodController routes:
+ */
 Route::get('foods', [FoodController::class, 'index']);
 Route::post('foods', [FoodController::class, 'store']);
+
+/**
+ * UserController routes:
+ */
 Route::get('users', [UserController::class, 'index']);
 Route::post('users', [UserController::class, 'store']);
