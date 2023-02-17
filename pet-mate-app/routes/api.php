cdb5b02929c5ee187c2d7c9414d1d291ceed0b75
Facu <105363828+Facundoblanco10@users.noreply.github.com>
2023-02-16 23:40:43 -0300

    Creating login, register and logout with sanctum and handling errors (#3)
    * Creating login, register and logout with sanctum
    Handling errors
    
    * Update pet-mate-app/app/Http/Controllers/AuthenticateController.php
    
    Co-authored-by: Nacho <46456405+ignaciocastro347@users.noreply.github.com>
    
    * Update pet-mate-app/app/Http/Controllers/AuthenticateController.php
    
    Co-authored-by: Nacho <46456405+ignaciocastro347@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nacho <46456405+ignaciocastro347@users.noreply.github.com>

--- pet-mate-app/routes/api.php
+++ pet-mate-app/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\FeederUserController;
 use App\Http\Controllers\FeedingTimeController;
 use App\Http\Controllers\FoodController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthenticateController;
 use App\Http\Controllers\RecordController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,58 +24,71 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-/**
- * FeederController routes:
- */
-Route::get('feeders', [FeederController::class, 'index']);
-Route::post('feeders', [FeederController::class, 'store']);
-Route::get('feeders/{feeder}', [FeederController::class, 'show']);
-Route::put('feeders/{feeder}', [FeederController::class, 'update']);
-Route::delete('feeders/{feeder}', [FeederController::class, 'destroy']);
-Route::get('feeders/{feeder}/users', [FeederController::class, 'show_users']);
-Route::get('feeders/get_id/{code_id}', [FeederController::class, 'get_id']);
-Route::get('feeders/{feeder}/feeding_times', [FeederController::class, 'show_feeding_times']);
-Route::get('feeders/{feeder}/records', [FeederController::class, 'show_records']);
-
 
 /**
- * UserFeederUserController routes:
+ * AuthenticateController routes:
  */
-Route::get('feeder_user', [FeederUserController::class, 'index']);
+Route::post('register', [AuthenticateController::class, 'register']);
+Route::post('login', [AuthenticateController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    
+    /**
+     * FeederController routes:
+     */
+    Route::get('feeders', [FeederController::class, 'index']);
+    Route::post('feeders', [FeederController::class, 'store']);
+    Route::get('feeders/{feeder}', [FeederController::class, 'show']);
+    Route::put('feeders/{feeder}', [FeederController::class, 'update']);
+    Route::delete('feeders/{feeder}', [FeederController::class, 'destroy']);
+    Route::get('feeders/{feeder}/users', [FeederController::class, 'show_users']);
+    Route::get('feeders/get_id/{code_id}', [FeederController::class, 'get_id']);
+    Route::get('feeders/{feeder}/feeding_times', [FeederController::class, 'show_feeding_times']);
+    Route::get('feeders/{feeder}/records', [FeederController::class, 'show_records']);
 
-/**
- * FeedingTimeController routes:
- */
-Route::get('feeding_times', [FeedingTimeController::class, 'index']);
-Route::post('feeding_times', [FeedingTimeController::class, 'store']);
-Route::get('feeding_times/{feeding_time}', [FeedingTimeController::class, 'show']);
-Route::put('feeding_times/{feeding_time}', [FeedingTimeController::class, 'update']);
-Route::delete('feeding_times/{feeding_time}', [FeedingTimeController::class, 'destroy']);
+    /**
+     * UserFeederUserController routes:
+     */
+    Route::get('feeder_user', [FeederUserController::class, 'index']);
 
-/**
- * FoodController routes:
- */
-Route::get('foods', [FoodController::class, 'index']);
-Route::post('foods', [FoodController::class, 'store']);
-Route::get('foods/{food}', [FoodController::class, 'show']);
-Route::put('foods/{food}', [FoodController::class, 'update']);
-Route::delete('foods/{food}', [FoodController::class, 'destroy']);
+    /**
+     * FeedingTimeController routes:
+     */
+    Route::get('feeding_times', [FeedingTimeController::class, 'index']);
+    Route::post('feeding_times', [FeedingTimeController::class, 'store']);
+    Route::get('feeding_times/{feeding_time}', [FeedingTimeController::class, 'show']);
+    Route::put('feeding_times/{feeding_time}', [FeedingTimeController::class, 'update']);
+    Route::delete('feeding_times/{feeding_time}', [FeedingTimeController::class, 'destroy']);
 
-/**
- * UserController routes:
- */
-Route::get('users', [UserController::class, 'index']);
-Route::post('users', [UserController::class, 'store']);
-Route::get('users/{user}', [UserController::class, 'show']);
-Route::put('users/{user}', [UserController::class, 'update']);
-Route::delete('users/{user}', [UserController::class, 'destroy']);
-Route::get('users/{user}/feeders', [UserController::class, 'show_feeders']);
+    /**
+     * FoodController routes:
+     */
+    Route::get('foods', [FoodController::class, 'index']);
+    Route::post('foods', [FoodController::class, 'store']);
+    Route::get('foods/{food}', [FoodController::class, 'show']);
+    Route::put('foods/{food}', [FoodController::class, 'update']);
+    Route::delete('foods/{food}', [FoodController::class, 'destroy']);
 
-/**
- * RecordController routes:
- */
-Route::get('records', [RecordController::class, 'index']);
-Route::post('records', [RecordController::class, 'store']);
-Route::get('records/{record}', [RecordController::class, 'show']);
-Route::put('records/{record}', [RecordController::class, 'update']);
-Route::delete('records/{record}', [RecordController::class, 'destroy']);
+    /**
+     * UserController routes:
+     */
+    Route::get('users', [UserController::class, 'index']);
+    Route::post('users', [UserController::class, 'store']);
+    Route::get('users/{user}', [UserController::class, 'show']);
+    Route::put('users/{user}', [UserController::class, 'update']);
+    Route::delete('users/{user}', [UserController::class, 'destroy']);
+    Route::get('users/{user}/feeders', [UserController::class, 'show_feeders']);
+    
+    /**
+    * RecordController routes:
+    */
+    Route::get('records', [RecordController::class, 'index']);
+    Route::post('records', [RecordController::class, 'store']);
+    Route::get('records/{record}', [RecordController::class, 'show']);
+    Route::put('records/{record}', [RecordController::class, 'update']);
+    Route::delete('records/{record}', [RecordController::class, 'destroy']);
+    
+    /**
+     * Logout
+     */
+    Route::post('logout', [AuthenticateController::class, 'logout']);
+});
