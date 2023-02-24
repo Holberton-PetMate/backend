4c9bf7186eb5226b3801f5b41b8c2db74ca6b677
Ignacio Castro <ignaciocastro347@gmail.com>
2023-02-24 16:17:04 -0300

    Fix

--- pet-mate-app/app/Http/Middleware/VerifyCsrfToken.php
+++ pet-mate-app/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'feeders/{feeder}/feeding_records'
+        '/api/users/{user}/redeem/{feeder}'
     ];
 }
